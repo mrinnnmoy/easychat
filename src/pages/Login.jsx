f443b410323a41f1effdda6658894422b1fb4350
mrinnnmoy <mrinmoy202000@gmail.com>
2022-12-17 10:58:35 +0530

    build login-page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">EasyChat</span>
+                <span className="title">Log In</span>
+                <form>
+                    <input required type="email" placeholder="Enter Email" />
+                    <input required type="password" placeholder="Enter Password" />
+                    <button>Log In</button>
+                </form>
+                <p>
+                    You don't have an account? Sign Up
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
